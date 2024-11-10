0dabddba49fd350f3c88bb819c637a3862d85f90
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-11 05:24:43 +0900

    feat: 사용자 주문 서비스 코드 작성

--- src/main/java/store/service/OrderService.java
+++ src/main/java/store/service/OrderService.java
@@ -1,14 +1,22 @@
 package store.service;
 
 import static store.constants.ErrorMessage.INVALID_PRODUCT_NAME;
-import static store.constants.ErrorMessage.INVALID_PRODUCT_QUANTITY;
+import static store.constants.ErrorMessage.INVALID_INPUT;
 import static store.constants.ErrorMessage.OVER_STOCK_QUANTITY;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import store.domain.Order;
+import store.domain.OrderItem;
+import store.domain.OrderItems;
+import store.domain.Product;
+import store.dto.CurrentStockInfo;
 import store.repository.ProductRepository;
 import store.utils.validator.OrderValidator;
 
@@ -19,20 +27,32 @@ public class OrderService {
         this.productRepository = productRepository;
     }
 
-    public HashMap<String, Order> createOrder(String userOrderItem) {
+    public OrderItems createOrder(String userOrderItem) {
         List<String> beforeCheckOrderItems = Arrays.asList(userOrderItem.trim().split(","));
         return makeOrder(beforeCheckOrderItems);
     }
 
-    private HashMap<String, Order> makeOrder(List<String> beforeCheckOrderItems) {
-        HashMap<String, Order> afterCheckOrderItems = new HashMap<>();
+    private OrderItems makeOrder(List<String> beforeCheckOrderItems) {
+        LinkedHashMap<String, Order> afterCheckOrderItems = new LinkedHashMap<>();
+
         for (String beforeCheckOrderItem : beforeCheckOrderItems) {
             OrderValidator.validateCorrectForm(beforeCheckOrderItem);
             List<String> orderItem = getOrderItem(beforeCheckOrderItem);
             addOrder(orderItem, afterCheckOrderItems);
         }
         checkItemQuantityInStore(afterCheckOrderItems);
-        return afterCheckOrderItems;
+        return convertToOrderItems(afterCheckOrderItems);
+    }
+
+    private OrderItems convertToOrderItems(LinkedHashMap<String, Order> afterCheckOrderItems) {
+        Set<String> names = afterCheckOrderItems.keySet();
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (String name : names) {
+            Product product = productRepository.findByKey(name);
+            OrderItem orderItem = new OrderItem(product, name, afterCheckOrderItems.get(name).getQuantity(), DateTimes.now());
+            orderItems.add(orderItem);
+        }
+        return new OrderItems(orderItems);
     }
 
     private List<String> getOrderItem(String beforeCheckOrderItem) {
@@ -47,18 +67,18 @@ public class OrderService {
         String productName = orderItem.getFirst();
         checkItemNameInStore(productName);
 
-        int quantity = orderQuantity(orderItem);
+        int quantity = orderQuantity(orderItem.getLast());
         if (quantity == 0) {
-            throw new IllegalArgumentException(INVALID_PRODUCT_QUANTITY.getMessage());
+            throw new IllegalArgumentException(INVALID_INPUT.getMessage());
         }
         addOrderQuantity(afterCheckOrderItems, productName, quantity);
     }
 
-    private int orderQuantity(List<String> orderItem) {
+    private int orderQuantity(String orderQuantity) {
         try {
-            return Integer.parseInt(orderItem.getLast());
+            return Integer.parseInt(orderQuantity);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(INVALID_PRODUCT_QUANTITY.getMessage());
+            throw new IllegalArgumentException(INVALID_INPUT.getMessage());
         }
     }
 
@@ -79,12 +99,12 @@ public class OrderService {
         for (Order order : afterCheckOrderItems.values()) {
             String productName = order.getName();
             int userQuantity = order.getQuantity();
-            int productQuantity = productRepository.getStockQuantity(productName);
-
+            Product product = productRepository.findByKey(productName);
+            CurrentStockInfo currentStockInfo = product.getCurrentStockInfo();
+            int productQuantity = currentStockInfo.getOriginalQuantity() + currentStockInfo.getPromotionQuantity();
             if (productQuantity < userQuantity) {
                 throw new IllegalArgumentException(OVER_STOCK_QUANTITY.getMessage());
             }
         }
     }
-
 }
