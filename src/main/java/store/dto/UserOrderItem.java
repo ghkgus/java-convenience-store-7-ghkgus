bc38213b3a5dbad659553d255a9f295549c8e114
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-11 05:10:00 +0900

    feat: 사용자 주문 정보 담는 dto 작성

--- src/main/java/store/dto/UserOrderItem.java
+++ src/main/java/store/dto/UserOrderItem.java
@@ -1,19 +1,60 @@
 package store.dto;
 
+import store.domain.GiftItemType;
+
 public class UserOrderItem {
     private String name;
-    private int quantity;
 
-    public UserOrderItem(String name, int quantity) {
+    private GiftItemType type;
+    private int totalQuantity;
+    private int giftQuantity;
+    private int nonGiftQuantity;
+
+
+    public UserOrderItem(String name, GiftItemType type, int quantity, int giftQuantity, int nonGiftQuantity) {
         this.name = name;
-        this.quantity = quantity;
+        this.type = type;
+        this.totalQuantity = quantity;
+        this.giftQuantity = giftQuantity;
+        this.nonGiftQuantity = nonGiftQuantity;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getQuantity() {
-        return quantity;
+    @Override
+    public String toString() {
+        return "UserOrderItem{" +
+                "name='" + name + '\'' +
+                ", type=" + type +
+                ", totalQuantity=" + totalQuantity +
+                ", giftQuantity=" + giftQuantity +
+                ", nonGiftQuantity=" + nonGiftQuantity +
+                '}';
+    }
+
+    public GiftItemType getType() {
+        return type;
+    }
+
+    public int getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public int getGiftQuantity() {
+        return giftQuantity;
+    }
+
+    public int getNonGiftQuantity() {
+        return nonGiftQuantity;
+    }
+
+    public void setTotalQuantity(int totalQuantity) {
+        this.totalQuantity = totalQuantity;
+    }
+
+    public void setGiftQuantity(int giftQuantity) {
+        this.giftQuantity = giftQuantity;
     }
 }
