aeab077e92f6cc9fec66ef66607e227ca7329e4d
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-11 21:47:29 +0900

    refactor: 파일 관련 상수 추가

--- src/main/java/store/constants/fileConstants/FileConstants.java
+++ src/main/java/store/constants/fileConstants/FileConstants.java
@@ -7,6 +7,11 @@ public class FileConstants {
     public final static int PRODUCT_FILE_SIZE = 4;
     public final static int PROMOTION_FILE_SIZE = 5;
 
+    public final static String DATE_FORMAT = "yyyy-MM-dd";
+    public final static int END_HOUR = 23;
+    public final static int END_MIN = 59;
+    public final static int END_SEC = 59;
+
     private FileConstants() {
 
     }
--- src/main/java/store/constants/fileConstants/FileErrorMessage.java
+++ src/main/java/store/constants/fileConstants/FileErrorMessage.java
@@ -4,6 +4,9 @@ public enum FileErrorMessage {
     INVALID_FILE("파일이 존재하지 않습니다."),
     UNREADABLE_FILE("파일을 읽을 수 없습니다."),
     INVALID_FILE_CONTENT_FORM("파일의 형식이 잘못되었습니다."),
+
+    IS_NOT_INTEGER_RANGE("정수범위의 가격이 아닙니다."),
+    IS_NOT_CORRECT_DATE_FORM("날짜 형식이 올바르지 않습니다.")
     ;
 
     private final String message;
@@ -14,4 +17,5 @@ public enum FileErrorMessage {
 
     public String getMessage() {
         return "[ERROR] " + message;
-    }}
+    }
+}
