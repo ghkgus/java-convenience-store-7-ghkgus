c114c0bb63f288316950fb256529b4ec84737460
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-11 21:46:28 +0900

    feat: 주문 관련 상수 역할 분리

---
+++ src/main/java/store/constants/OrderConstants.java
@@ -0,0 +1,15 @@
+package store.constants;
+
+public class OrderConstants {
+    public static final String OUT_OF_STOCK = "재고 없음";
+
+    public static final String CONSECUTIVE_COMMA = ",,";
+    public static final int PRODUCT_AND_QUANTITY_SIZE = 2;
+    public static final String LEFT_SQUARE_BRACKET= "[";
+    public static final String RIGHT_SQUARE_BRACKET = "]";
+
+    public static final String HYPHEN = "-";
+    private OrderConstants() {
+
+    }
+}
