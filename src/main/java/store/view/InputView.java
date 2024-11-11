f060433b21102d7b818c9f3a11d0a7d245ee1365
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-11 20:17:50 +0900

    refactor: 메시지관련 책임 분리

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,32 +1,33 @@
 package store.view;
 
+import static store.constants.InputMessage.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String getUserOrder() {
-        System.out.println("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println(GET_USER_ORDER_MESSAGE.getMessage());
         return Console.readLine();
     }
 
     public String askUserForRegularPricePayment(String name, int nonGiftQuantity) {
-        System.out.printf("\n현재 %s %,d 개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", name, nonGiftQuantity);
+        System.out.printf(ASK_USER_FOR_PAY_REGULAR_PRICE.getMessage(), name, nonGiftQuantity);
         return Console.readLine();
     }
 
     public String askUserForExtraGift(String name) {
-        System.out.printf("\n현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", name);
+        System.out.printf(ASK_USER_FOR_EXTRA_FREE.getMessage(), name);
         return Console.readLine();
     }
 
     public String askUserToApplyMembership() {
         System.out.println();
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        System.out.println(ASK_USER_FOR_MEMBERSHIP_DISCOUNT.getMessage());
         return Console.readLine();
     }
 
     public String askUserToRepurchase() {
         System.out.println();
-        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        System.out.println(ASK_USER_FOR_REPURCHASE.getMessage());
         return Console.readLine();
     }
 }
