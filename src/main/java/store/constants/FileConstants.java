0bed2d779d910497dc81637f762b75f8b732e80e
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-11 17:23:06 +0900

    refactor: 파일 관련 상수 관리하는 클래스 작성

---
+++ src/main/java/store/constants/FileConstants.java
@@ -0,0 +1,13 @@
+package store.constants;
+
+public class FileConstants {
+
+    public final static String SPLIT_REGEX = ",";
+    public final static String NON_PROMOTION = "null";
+    public final static int PRODUCT_FILE_SIZE = 4;
+    public final static int PROMOTION_FILE_SIZE = 5;
+
+    private FileConstants() {
+
+    }
+}
