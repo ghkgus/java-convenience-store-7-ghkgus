f290aa5fa9732dd7a463eb5d52e153db2557687c
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-11 19:40:40 +0900

    feat: 파일 입력시 오류 메시지 작성

--- src/main/java/store/constants/FileErrorMessage.java
+++ src/main/java/store/constants/FileErrorMessage.java
@@ -2,8 +2,8 @@ package store.constants;
 
 public enum FileErrorMessage {
     INVALID_FILE("파일이 존재하지 않습니다."),
-    UNREADABLE_FILE("[ERROR] 파일을 읽을 수 없습니다."),
-    INVALID_FILE_CONTENT_FORM("[ERROR] 파일의 형식이 잘못되었습니다."),
+    UNREADABLE_FILE("파일을 읽을 수 없습니다."),
+    INVALID_FILE_CONTENT_FORM("파일의 형식이 잘못되었습니다."),
     ;
 
     private final String message;
